ecb948923a2fe1c68715f0c2393dce72ae67fc4c
Leroy <roylee1920@hotmail.com>
2016-08-20 23:50:01 -0400

    generated keystonejs

---
+++ keystone.js
@@ -0,0 +1,55 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+
+keystone.init({
+	'name': 'Computer Graphics Club',
+	'brand': 'Computer Graphics Club',
+
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'jade',
+
+	'emails': 'templates/emails',
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'Member',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+keystone.set('email locals', {
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7',
+		},
+	},
+});
+keystone.set('email tests', require('./routes/emails'));
+
+keystone.set('nav', {
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	members: 'members',
+});
+
+keystone.start();
